d5f71aa97fdad5cb64b548bee724a1598f7fbe5f
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-09-22 19:50:12 +0100

    test: continuation

--- test/test_monge_ampere.py
+++ test/test_monge_ampere.py
@@ -1,10 +1,6 @@
-from firedrake import *
-from movement import monge_ampere
+from movement import *
+from monitors import *
 import pytest
-
-
-def monitor_function(mesh):
-    return Constant(1.0)
 
 
 @pytest.fixture(params=['relaxation', 'quasi_newton'])
@@ -12,9 +8,56 @@ def method(request):
     return request.param
 
 
-def test_uniform_monitor(method):
-    mesh = UnitSquareMesh(10, 10)
+def test_uniform_monitor(method, exports=False):
+    """
+    Test that the mesh mover converges in one
+    iteration for a constant monitor function.
+    """
+    n = 10
+    mesh = UnitSquareMesh(n, n)
     coords = mesh.coordinates.dat.data.copy()
-    num_iterations = monge_ampere(mesh, monitor_function, method=method)
+
+    mover = MongeAmpereMover(mesh, const_monitor, method=method)
+    num_iterations = mover.adapt()
+
     assert np.allclose(coords, mesh.coordinates.dat.data)
     assert num_iterations == 1
+
+
+def test_continue(method, exports=False):
+    """
+    Test that providing a good initial guess
+    benefits the solver.
+    """
+    n = 20
+    mesh = UnitSquareMesh(n, n)
+    rtol = 1.0e-03
+
+    # Solve the problem to a weak tolerance
+    mover = MongeAmpereMover(mesh, ring_monitor, method=method, rtol=0.1)
+    num_it_init = mover.adapt()
+    phi, sigma = mover.phi, mover.sigma
+    if exports:
+        File("outputs/init.pvd").write(phi, sigma)
+
+    # Continue with a new Mover
+    mover = MongeAmpereMover(mesh, ring_monitor, method=method, rtol=rtol,
+                             phi_init=phi, sigma_init=sigma)
+    num_it_continue = mover.adapt()
+    if exports:
+        File("outputs/continue.pvd").write(mover.phi, mover.sigma)
+
+    # Solve the problem again to a tight tolerance
+    mesh = UnitSquareMesh(n, n)
+    mover = MongeAmpereMover(mesh, ring_monitor, method=method, rtol=rtol)
+    num_it_naive = mover.adapt()
+    if exports:
+        File("outputs/naive.pvd").write(mover.phi, mover.sigma)
+
+    assert num_it_continue < num_it_naive
+    assert num_it_init + num_it_continue < num_it_naive
+
+
+if __name__ == "__main__":
+    test_continue('relaxation', exports=True)
+    # test_continue('quasi_newton', exports=True)
