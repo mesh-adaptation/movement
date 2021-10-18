ed309d1fd69f08791afecd6f428e44cdc40ff125
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-10-18 13:09:44 +0100

    mover: get_coordinate_section, etc.

--- movement/mover.py
+++ movement/mover.py
@@ -1,4 +1,6 @@
 import firedrake
+from firedrake.cython.dmcommon import create_section
+import numpy as np
 
 
 __all__ = ["PrimeMover"]
@@ -14,6 +16,10 @@ class PrimeMover(object):
         self.dim = mesh.topological_dimension()
         if self.dim != 2:
             raise NotImplementedError(f"Dimension {self.dim} has not been considered yet")
+        self.gdim = mesh.geometric_dimension()
+        self.plex = self.mesh.topology_dm
+        self.vertex_indices = self.plex.getDepthStratum(0)
+        self.edge_indices = self.plex.getDepthStratum(1)
 
         # Measures
         degree = kwargs.get('quadrature_degree')
@@ -24,6 +30,47 @@ class PrimeMover(object):
         # Mesh coordinate functions
         self._x = firedrake.Function(self.mesh.coordinates, name="Physical coordinates")
         self.xi = firedrake.Function(self.mesh.coordinates, name="Computational coordinates")
+
+    def _get_coordinate_section(self):
+        entity_dofs = np.zeros(self.dim+1, dtype=np.int32)
+        entity_dofs[0] = self.gdim
+        self._coordinate_section = create_section(self.mesh, entity_dofs)
+        dm_coords = self.plex.getCoordinateDM()
+        dm_coords.setDefaultSection(self._coordinate_section)
+        coords_local = dm_coords.createLocalVec()
+        coords_local.array[:] = np.reshape(self.mesh.coordinates.dat.data_with_halos,
+                                           coords_local.array.shape)
+        self.plex.setCoordinatesLocal(coords_local)
+
+    def _get_edge_vector_section(self):
+        entity_dofs = np.zeros(self.dim+1, dtype=np.int32)
+        entity_dofs[1] = 1
+        self._edge_vector_section = create_section(self.mesh, entity_dofs)
+
+    def coordinate_offset(self, index):
+        """
+        Get the DMPlex coordinate section offset
+        for a given `index`.
+        """
+        if not hasattr(self, '_coordinate_section'):
+            self._get_coordinate_section()
+        return self._coordinate_section.getOffset(index)//self.dim
+
+    def edge_vector_offset(self, index):
+        """
+        Get the DMPlex edge vector section offset
+        for a given `index`.
+        """
+        if not hasattr(self, '_edge_vector_section'):
+            self._get_edge_vector_section()
+        return self._edge_vector_section.getOffset(index)
+
+    def coordinate(self, index):
+        """
+        Get the mesh coordinate associated with
+        a given `index`.
+        """
+        return self.mesh.coordinates.dat.data_with_halos[self.get_offset(index)]
 
     def adapt(self):
         raise NotImplementedError("Implement `adapt` in the derived class.")
