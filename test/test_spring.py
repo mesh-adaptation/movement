ec6b2fd573c554e9517bf9b89928884af0c1f5aa
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-10-18 16:22:24 +0100

    test: facet areas, tangents, angles

--- test/test_spring.py
+++ test/test_spring.py
@@ -1,4 +1,4 @@
-from firedrake import SquareMesh
+from firedrake import SquareMesh, UnitTriangleMesh
 from movement import SpringMover
 import numpy as np
 import pytest
@@ -12,6 +12,43 @@ def method(request):
 @pytest.fixture(params=[0.0, 0.25])
 def time(request):
     return request.param
+
+
+def test_facet_areas():
+    """
+    Test that the edge lenths of a right-angled
+    triangle are computed correctly.
+    """
+    mesh = UnitTriangleMesh()
+    mover = SpringMover(mesh)
+    facet_areas = mover.facet_areas.dat.data
+    expected = [1, np.sqrt(2), 1]
+    assert np.allclose(facet_areas, expected)
+
+
+def test_tangents():
+    """
+    Test that the tangent vectors of a right-angled
+    triangle are computed correctly.
+    """
+    mesh = UnitTriangleMesh()
+    mover = SpringMover(mesh)
+    tangents = mover.tangents.dat.data
+    expected = [[1, 0], [np.sqrt(2)/2, np.sqrt(2)/2], [0, 1]]
+    assert np.allclose(np.abs(tangents), expected)
+
+
+def test_angles():
+    """
+    Test that the arguments (angles from the
+    x-axis) of a right-angled triangle are
+    computed correctly.
+    """
+    mesh = UnitTriangleMesh()
+    mover = SpringMover(mesh)
+    angles = 180*mover.angles.dat.data/np.pi
+    expected = [0, 135, 90]
+    assert np.allclose(angles, expected)
 
 
 def test_forced(method, time):
