dd9a038834c9173e6c157285b5d38839ee531109
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-10-19 09:53:28 +0100

    test: simplify test_spring::test_forced

--- movement/spring.py
+++ movement/spring.py
@@ -217,7 +217,7 @@ class SpringMover_Lineal(SpringMover_Base):
         # Update mesh coordinates
         shape = self.mesh.coordinates.dat.data_with_halos.shape
         self.mesh.coordinates.dat.data_with_halos[:] += self.displacement.reshape(shape)
-        # TODO: Update DMPlex coordinates, too
+        # self._update_plex_coordinates()
 
 
 class SpringMover_Torsional(SpringMover_Lineal):
--- test/test_spring.py
+++ test/test_spring.py
@@ -1,4 +1,4 @@
-from firedrake import SquareMesh, UnitTriangleMesh
+import firedrake
 from movement import SpringMover
 import numpy as np
 import pytest
@@ -19,7 +19,7 @@ def test_facet_areas():
     Test that the edge lenths of a right-angled
     triangle are computed correctly.
     """
-    mesh = UnitTriangleMesh()
+    mesh = firedrake.UnitTriangleMesh()
     mover = SpringMover(mesh)
     facet_areas = mover.facet_areas.dat.data
     expected = [1, np.sqrt(2), 1]
@@ -31,7 +31,7 @@ def test_tangents():
     Test that the tangent vectors of a right-angled
     triangle are computed correctly.
     """
-    mesh = UnitTriangleMesh()
+    mesh = firedrake.UnitTriangleMesh()
     mover = SpringMover(mesh)
     tangents = mover.tangents.dat.data
     expected = [[1, 0], [np.sqrt(2)/2, np.sqrt(2)/2], [0, 1]]
@@ -44,18 +44,23 @@ def test_angles():
     x-axis) of a right-angled triangle are
     computed correctly.
     """
-    mesh = UnitTriangleMesh()
+    mesh = firedrake.UnitTriangleMesh()
     mover = SpringMover(mesh)
     angles = 180*mover.angles.dat.data/np.pi
     expected = [0, 135, 90]
     assert np.allclose(angles, expected)
 
 
-def test_forced(method, time):
+def test_forced(method, time, plot=False, test=True):
     """
     Test that a uniform mesh is moved as expected
     when only one of its boundaries is forced.
     """
+    it = 0
+    if not np.isclose(time, 0.0):
+        it = 1/time
+        assert np.isclose(np.round(it), it), "The time needs to be a reciprocal of an integer"
+        it = int(np.round(it))
 
     # Set parameters
     n = 10   # Mesh resolution
@@ -63,37 +68,40 @@ def test_forced(method, time):
     T = 1.0  # Forcing period
 
     # Construct mesh and mover
-    mesh = SquareMesh(n, n, 2)
+    mesh = firedrake.SquareMesh(n, n, 2)
+    V = mesh.coordinates.function_space()
     coords = mesh.coordinates.dat.data.copy()
     bnd = mesh.exterior_facets
-    mover = SpringMover(mesh, method)
+    mover = SpringMover(mesh, method=method)
 
     def update_forcings(t):
         """
         Sinusoidal forcing on the top boundary.
         """
-        forced_nodes = list(bnd.subset(4).indices)
-        for e in range(*mover.edge_indices):
-            i, j = (mover.coordinate_offset(v) for v in mover.plex.getCone(e))
-            facet_num = bnd.point2facetnumber[e]
-            if facet_num in forced_nodes:
-                mover.f.dat.data[i][1] += A*np.sin(2*np.pi*t/T)*np.sin(np.pi*coords[i][0])
-                mover.f.dat.data[j][1] += A*np.sin(2*np.pi*t/T)*np.sin(np.pi*coords[j][0])
+        for i in firedrake.DirichletBC(V, 0, 4).nodes:
+            mover.f.dat.data[i][1] += A*np.sin(2*np.pi*t/T)*np.sin(np.pi*coords[i][0])
 
     # Move the mesh
     mover.move(time, update_forcings=update_forcings, fixed_boundaries=[1, 2, 3])
     new_coords = mover.mesh.coordinates.dat.data
-    if np.isclose(time, 0.0):
-        assert np.allclose(coords, new_coords)
-    else:
-        it = 1/time
-        assert np.isclose(np.round(it), it), "The time needs to be a reciprocal of an integer"
-        it = int(np.round(it))
-        assert np.allclose(new_coords, np.load(f"data/forced_mesh_lineal_{it}.npy"))
+
+    # Plotting
+    if plot:
+        import matplotlib.pyplot as plt
+
+        fig, axes = plt.subplots()
+        firedrake.triplot(mover.mesh, axes=axes)
+        axes.axis(False)
+        plt.tight_layout()
+        plt.savefig(f"plots/mesh_{method}_{it}.png")
+
+    # Check as expected
+    if test:
+        expected = coords if np.isclose(time, 0.0) else np.load(f"data/forced_mesh_lineal_{it}.npy")
+        assert np.allclose(new_coords, expected)
     return mover.mesh
 
 
 if __name__ == "__main__":
-    it = 4
-    mesh = test_forced('lineal', 1/it)
-    np.save(f"data/forced_mesh_lineal_{it}", mesh.coordinates.dat.data)
+    mesh = test_forced('lineal', 0.25, plot=True, test=False)
+    np.save(f"data/forced_mesh_lineal_4", mesh.coordinates.dat.data)
