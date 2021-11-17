d4b0ea3cf6869bac504160ea5564e7638e6c7965
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2021-11-17 11:03:28 +0000

    test: test_tangling

---
+++ test/test_tangling.py
@@ -0,0 +1,15 @@
+from firedrake import *
+from movement import *
+
+
+def test_tangling_checker():
+    """
+    Test that :class:`MeshTanglingChecker`
+    can correctly identify tangled elements
+    in a 2D triangular mesh.
+    """
+    mesh = UnitSquareMesh(3, 3)
+    checker = MeshTanglingChecker(mesh)
+    assert checker.check() == 0
+    mesh.coordinates.dat.data[3] += 0.2
+    assert checker.check() == 1
