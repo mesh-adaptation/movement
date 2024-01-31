9687dee74ea2c803857584806b7ec122174a5c93
Stephan Kramer <s.kramer@imperial.ac.uk>
2024-01-31 10:17:37 +0000

    Addresses PR review

--- demos/monge_ampere_helmholtz.py
+++ demos/monge_ampere_helmholtz.py
@@ -1,14 +1,12 @@
 # Using mesh movement to optimize the mesh for PDE solution
 # =========================================================
 #
-# In this demo will demonstrate how we might use mesh movement
-# to obtain a mesh that is optimized for solving a particular
-# PDE. The general idea is that we want to reduce numerical
-# error by increasing resolution where the local error is
-# (expected to be) high and decrease it elsewhere.
+# In this demo will demonstrate how we might use mesh movement to obtain a mesh
+# that is optimized for solving a particular PDE. The general idea is that we
+# want to reduce numerical error by increasing resolution where the local error
+# is (expected to be) high and decrease it elsewhere.
 #
-# As an example we will look at the discretisation of the
-# Helmholtz equation
+# As an example we will look at the discretisation of the Helmholtz equation
 #
 # .. math::
 #
@@ -18,25 +16,29 @@
 #
 # For an explanation of how we can use Firedrake to implement a Finite Element
 # Method (FEM) discretisation of this PDE see the `corresponding Firedrake demo
-# <https://www.firedrakeproject.org/demos/helmholtz.py.html>`_. The only changes
-# we introduce is that we choose a different, slightly more interesting
+# <https://www.firedrakeproject.org/demos/helmholtz.py.html>`_. The only
+# changes we introduce is that we choose a different, slightly more interesting
 # solution :math:`u` and rhs :math:`f`
 #
 # .. math::
 #
-#    u(x, y) &= \exp(-((x-0.5)^2 + (y-0.5)^2))
+#    u(x, y) &= \exp\left(-\frac{(x-x_0)^2 + (y-y_0)^2}{w^2}\right)
 #
-#    f(x, y) &= \left[ -\frac{4(x-0.5)^2 + 4(y-0.5)^2}{w^4} + \frac{4}{w^2} + 1 \right]
-#               \exp(-((x-0.5)^2 + (y-0.5)^2))
+#    f(x, y) &= -\nabla^2 u(x, y) + u(x, y)
+#        = \left[ -\frac{4(x-x_0)^2 + 4(y-y_0)^2}{w^4}
+#                + \frac{4}{w^2} + 1 \right]
+#          \exp\left(-\frac{(x-x_0)^2 +  (y-y_0)^2}{w^2}\right)
 #
-# Note that here we *first* choose the solution :math:`u` after which we can compute what
-# rhs :math:`f` should be, by substitution in the PDE, in order for :math:`u` to be the
-# analytical solution. This so called Method of Manufactured Solutions approach is an
-# easy way to construct PDE configurations for which we know the analytical solution,
-# e.g. for testing purposes.
+# where :math:`(x_0, y_0)` is the centre of the Gaussian with width :math:`w`.
+# Note that here we *first* choose the solution :math:`u` after which we can
+# compute what rhs :math:`f` should be, by substitution in the PDE, in order
+# for :math:`u` to be the analytical solution. This so called Method of
+# Manufactured Solutions approach is an easy way to construct PDE
+# configurations for which we know the analytical solution, e.g. for testing
+# purposes.
 #
 # Based on the code in the Firedrake demo, we first solve the PDE on a uniform mesh.
-# Because our chosen solution does not satisfy homogenoues Neumann boundary conditions,
+# Because our chosen solution does not satisfy homogeneous Neumann boundary conditions,
 # we instead apply Dirichlet boundary conditions based on the chosen analytical solution.
 
 from firedrake import *
@@ -89,6 +91,10 @@ plt.savefig("monge_ampere_helmholtz-initial_solution.jpg")
 error = u_h - u_exact(mesh)
 print("L2-norm error on initial mesh:", sqrt(assemble(dot(error, error) * dx)))
 
+# .. code-block:: none
+#
+#    L2-norm error on initial mesh: 0.010233816824277465
+#
 # We will now try to use mesh movement to optimize the mesh to reduce
 # this numerical error. A good indicator for where resolution is required
 # is to look at the curvature of the solution which can be expressed
@@ -97,7 +103,7 @@ print("L2-norm error on initial mesh:", sqrt(assemble(dot(error, error) * dx)))
 #
 # .. math::
 #
-#    m = 1 + \alpha \frac{H(u_h):H(u_h)}{\max_{{\bf x}\in\Omega} H(u):H(u)}
+#    m = 1 + \alpha \frac{H(u_h):H(u_h)}{\max_{{\bf x}\in\Omega} H(u_h):H(u_h)}
 #
 # where :math:`:` indicates the inner product, i.e. :math:`\sqrt{H:H}` is the Frobenius norm
 # of :math:`H`. We have normalized such that the minimum of the monitor function is one (where
@@ -146,6 +152,32 @@ fig, axes = plt.subplots()
 mover = MongeAmpereMover(mesh, monitor, method="quasi_newton")
 mover.move()
 
+# For every iteration the MongeAmpereMover prints the minimum to maximum ratio of
+# the cell areas in the mesh, the residual in the Monge Ampere equation, and the
+# normalized standard deviation of cell areas (equidistribution):
+#
+# .. code-block:: none
+#
+#    0   Min/Max 2.0268e-01   Residual 4.7659e-01   Equidistribution 9.9384e-01
+#    1   Min/Max 3.7852e-01   Residual 2.4133e-01   Equidistribution 9.9659e-01
+#    2   Min/Max 5.9791e-01   Residual 1.2442e-01   Equidistribution 9.9774e-01
+#    3   Min/Max 7.1000e-01   Residual 6.5811e-02   Equidistribution 9.9804e-01
+#    4   Min/Max 7.7704e-01   Residual 3.4929e-02   Equidistribution 9.9818e-01
+#    5   Min/Max 8.3434e-01   Residual 1.7261e-02   Equidistribution 9.9829e-01
+#    6   Min/Max 8.5805e-01   Residual 7.7528e-03   Equidistribution 9.9833e-01
+#    7   Min/Max 8.6653e-01   Residual 3.1551e-03   Equidistribution 9.9835e-01
+#    8   Min/Max 8.6796e-01   Residual 1.1644e-03   Equidistribution 9.9835e-01
+#    9   Min/Max 8.6792e-01   Residual 3.8816e-04   Equidistribution 9.9835e-01
+#   10   Min/Max 8.6784e-01   Residual 1.1574e-04   Equidistribution 9.9835e-01
+#   11   Min/Max 8.6778e-01   Residual 1.5645e-05   Equidistribution 9.9835e-01
+#   12   Min/Max 8.6776e-01   Residual 7.5654e-06   Equidistribution 9.9835e-01
+#   13   Min/Max 8.6776e-01   Residual 3.5803e-06   Equidistribution 9.9835e-01
+#   14   Min/Max 8.6775e-01   Residual 1.5113e-06   Equidistribution 9.9835e-01
+#   15   Min/Max 8.6775e-01   Residual 5.7080e-07   Equidistribution 9.9835e-01
+#   16   Min/Max 8.6775e-01   Residual 1.9357e-07   Equidistribution 9.9835e-01
+#   17   Min/Max 8.6775e-01   Residual 5.8585e-08   Equidistribution 9.9835e-01
+#   Converged in 17 iterations.
+#
 # Plotting the resulting mesh
 
 from firedrake.pyplot import triplot
@@ -166,6 +198,10 @@ u_h = solve_helmholtz(mover.mesh)
 error = u_h - u_exact(mover.mesh)
 print("L2-norm error on moved mesh:", sqrt(assemble(dot(error, error) * dx)))
 
+# .. code-block:: none
+#
+#    L2-norm error on moved mesh: 0.005955820168534556
+#
 # Of course, in many practical problems we do not actually have access
 # to the exact solution. We can then use the Hessian of the numerical
 # solution in the monitor function. Calculating the Hessian we have to
@@ -206,3 +242,7 @@ mover.move()
 u_h = solve_helmholtz(mover.mesh)
 error = u_h - u_exact(mover.mesh)
 print("L2-norm error on moved mesh:", sqrt(assemble(dot(error, error) * dx)))
+
+# .. code-block:: none
+#
+#    L2-norm error on moved mesh: 0.00630874419681285
