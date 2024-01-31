5d2c7926b541b86f49b68748f82fc689ecfb47af
Stephan Kramer <s.kramer@imperial.ac.uk>
2024-01-31 18:43:09 +0000

    Change MA diagnostic labelling:
    equidistribution -> (Coefficient of) Variation

--- demos/monge_ampere_helmholtz.py
+++ demos/monge_ampere_helmholtz.py
@@ -154,28 +154,28 @@ mover.move()
 
 # For every iteration the MongeAmpereMover prints the minimum to maximum ratio of
 # the cell areas in the mesh, the residual in the Monge Ampere equation, and the
-# normalized standard deviation of cell areas (equidistribution):
+# coefficient of variation of the cell areas:
 #
 # .. code-block:: none
 #
-#    0   Min/Max 2.0268e-01   Residual 4.7659e-01   Equidistribution 9.9384e-01
-#    1   Min/Max 3.7852e-01   Residual 2.4133e-01   Equidistribution 9.9659e-01
-#    2   Min/Max 5.9791e-01   Residual 1.2442e-01   Equidistribution 9.9774e-01
-#    3   Min/Max 7.1000e-01   Residual 6.5811e-02   Equidistribution 9.9804e-01
-#    4   Min/Max 7.7704e-01   Residual 3.4929e-02   Equidistribution 9.9818e-01
-#    5   Min/Max 8.3434e-01   Residual 1.7261e-02   Equidistribution 9.9829e-01
-#    6   Min/Max 8.5805e-01   Residual 7.7528e-03   Equidistribution 9.9833e-01
-#    7   Min/Max 8.6653e-01   Residual 3.1551e-03   Equidistribution 9.9835e-01
-#    8   Min/Max 8.6796e-01   Residual 1.1644e-03   Equidistribution 9.9835e-01
-#    9   Min/Max 8.6792e-01   Residual 3.8816e-04   Equidistribution 9.9835e-01
-#   10   Min/Max 8.6784e-01   Residual 1.1574e-04   Equidistribution 9.9835e-01
-#   11   Min/Max 8.6778e-01   Residual 1.5645e-05   Equidistribution 9.9835e-01
-#   12   Min/Max 8.6776e-01   Residual 7.5654e-06   Equidistribution 9.9835e-01
-#   13   Min/Max 8.6776e-01   Residual 3.5803e-06   Equidistribution 9.9835e-01
-#   14   Min/Max 8.6775e-01   Residual 1.5113e-06   Equidistribution 9.9835e-01
-#   15   Min/Max 8.6775e-01   Residual 5.7080e-07   Equidistribution 9.9835e-01
-#   16   Min/Max 8.6775e-01   Residual 1.9357e-07   Equidistribution 9.9835e-01
-#   17   Min/Max 8.6775e-01   Residual 5.8585e-08   Equidistribution 9.9835e-01
+#    0   Min/Max 2.0268e-01   Residual 4.7659e-01   Variation (σ/μ) 9.9384e-01
+#    1   Min/Max 3.7852e-01   Residual 2.4133e-01   Variation (σ/μ) 9.9659e-01
+#    2   Min/Max 5.9791e-01   Residual 1.2442e-01   Variation (σ/μ) 9.9774e-01
+#    3   Min/Max 7.1000e-01   Residual 6.5811e-02   Variation (σ/μ) 9.9804e-01
+#    4   Min/Max 7.7704e-01   Residual 3.4929e-02   Variation (σ/μ) 9.9818e-01
+#    5   Min/Max 8.3434e-01   Residual 1.7261e-02   Variation (σ/μ) 9.9829e-01
+#    6   Min/Max 8.5805e-01   Residual 7.7528e-03   Variation (σ/μ) 9.9833e-01
+#    7   Min/Max 8.6653e-01   Residual 3.1551e-03   Variation (σ/μ) 9.9835e-01
+#    8   Min/Max 8.6796e-01   Residual 1.1644e-03   Variation (σ/μ) 9.9835e-01
+#    9   Min/Max 8.6792e-01   Residual 3.8816e-04   Variation (σ/μ) 9.9835e-01
+#   10   Min/Max 8.6784e-01   Residual 1.1574e-04   Variation (σ/μ) 9.9835e-01
+#   11   Min/Max 8.6778e-01   Residual 1.5645e-05   Variation (σ/μ) 9.9835e-01
+#   12   Min/Max 8.6776e-01   Residual 7.5654e-06   Variation (σ/μ) 9.9835e-01
+#   13   Min/Max 8.6776e-01   Residual 3.5803e-06   Variation (σ/μ) 9.9835e-01
+#   14   Min/Max 8.6775e-01   Residual 1.5113e-06   Variation (σ/μ) 9.9835e-01
+#   15   Min/Max 8.6775e-01   Residual 5.7080e-07   Variation (σ/μ) 9.9835e-01
+#   16   Min/Max 8.6775e-01   Residual 1.9357e-07   Variation (σ/μ) 9.9835e-01
+#   17   Min/Max 8.6775e-01   Residual 5.8585e-08   Variation (σ/μ) 9.9835e-01
 #   Converged in 17 iterations.
 #
 # Plotting the resulting mesh
