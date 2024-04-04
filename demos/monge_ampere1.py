df4f316b31283f8f39c337a426ad6b8428d3da7d
Joe Wallwork <joe.wallwork@outlook.com>
2024-04-04 09:19:39 +0100

    #50: Add zoom plot

--- demos/monge_ampere1.py
+++ demos/monge_ampere1.py
@@ -103,6 +103,20 @@ plt.savefig("monge_ampere1-adapted_mesh.jpg")
 #    :figwidth: 60%
 #    :align: center
 #
+# Whilst it looks like the mesh might have tangled elements, closer inspection shows
+# that this is not the case.
+
+fig, axes = plt.subplots()
+triplot(mover.mesh, axes=axes)
+axes.set_xlim([0.15, 0.3])
+axes.set_ylim([0.15, 0.3])
+axes.set_aspect(1)
+plt.savefig("monge_ampere1-adapted_mesh_zoom.jpg")
+
+# .. figure:: monge_ampere1-adapted_mesh_zoom.jpg
+#    :figwidth: 60%
+#    :align: center
+#
 # This tutorial can be dowloaded as a `Python script <monge_ampere1.py>`__.
 #
 #
