ecd42b5390fcd11c59096842a01155f0683e9a5f
Joe Wallwork <joe.wallwork@outlook.com>
2024-04-04 09:37:03 +0100

    #19: Unit tests for mesh tangling

--- test/test_tangling.py
+++ test/test_tangling.py
@@ -1,15 +1,25 @@
 from firedrake import *
 from movement import *
+import unittest
 
 
-def test_tangling_checker():
+class TestTangling(unittest.TestCase):
     """
-    Test that :class:`MeshTanglingChecker`
-    can correctly identify tangled elements
-    in a 2D triangular mesh.
+    Unit tests for mesh tangling checking.
     """
-    mesh = UnitSquareMesh(3, 3)
-    checker = MeshTanglingChecker(mesh, raise_error=False)
-    assert checker.check() == 0
-    mesh.coordinates.dat.data[3] += 0.2
-    assert checker.check() == 1
+
+    def test_tangling_checker_error(self):
+        mesh = UnitSquareMesh(3, 3)
+        checker = MeshTanglingChecker(mesh, raise_error=True)
+        mesh.coordinates.dat.data[3] += 0.2
+        with self.assertRaises(ValueError) as cm:
+            checker.check()
+        msg = "Mesh has 1 tangled elements."
+        self.assertEqual(str(cm.exception), msg)
+
+    def test_tangling_checker_warning(self):
+        mesh = UnitSquareMesh(3, 3)
+        checker = MeshTanglingChecker(mesh, raise_error=False)
+        self.assertEqual(checker.check(), 0)
+        mesh.coordinates.dat.data[3] += 0.2
+        self.assertEqual(checker.check(), 1)
