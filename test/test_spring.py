1c6425308b2c593afb8d904a2bc02083222077cb
Joe Wallwork <joe.wallwork@outlook.com>
2024-04-18 12:00:13 +0100

    #19: Rework Spring tests using unittest

--- test/test_spring.py
+++ test/test_spring.py
@@ -1,55 +1,43 @@
+import unittest
+
 import firedrake
 import numpy as np
-import pytest
 
 from movement import SpringMover
 
 
-@pytest.fixture(params=["lineal"])
-def method(request):
-    return request.param
-
-
-@pytest.fixture(params=[0.0, 0.25])
-def time(request):
-    return request.param
-
-
-def test_facet_areas():
+class TestQuantities(unittest.TestCase):
     """
-    Test that the edge lenths of a right-angled
-    triangle are computed correctly.
+    Unit tests for the element quantity utilities underlying the :class:`~.SpringMover`
+    classes.
     """
-    mesh = firedrake.UnitTriangleMesh()
-    timestep = 1.0
-    mover = SpringMover(mesh, timestep)
-    facet_areas = mover.facet_areas.dat.data
-    expected = [1, np.sqrt(2), 1]
-    assert np.allclose(facet_areas, expected)
 
+    def setUp(self):
+        self.mover = SpringMover(firedrake.UnitTriangleMesh(), 1.0)
 
-def test_tangents():
-    """
-    Test that the tangent vectors of a right-angled
-    triangle are computed correctly.
-    """
-    mesh = firedrake.UnitTriangleMesh()
-    timestep = 1.0
-    mover = SpringMover(mesh, timestep)
-    tangents = mover.tangents.dat.data
-    expected = [[1, 0], [np.sqrt(2) / 2, np.sqrt(2) / 2], [0, 1]]
-    assert np.allclose(np.abs(tangents), expected)
+    @staticmethod
+    def rad2deg(radians):
+        return 180 * radians / np.pi
 
+    def test_facet_areas(self):
+        """
+        Test that the edge lenths of a right-angled triangle are computed correctly.
+        """
+        facet_areas = self.mover.facet_areas.dat.data
+        self.assertTrue(np.allclose(facet_areas, [1, np.sqrt(2), 1]))
 
-def test_angles():
-    """
-    Test that the arguments (angles from the
-    x-axis) of a right-angled triangle are
-    computed correctly.
-    """
-    mesh = firedrake.UnitTriangleMesh()
-    timestep = 1.0
-    mover = SpringMover(mesh, timestep)
-    angles = 180 * mover.angles.dat.data / np.pi
-    expected = [0, 135, 90]
-    assert np.allclose(angles, expected)
+    def test_tangents(self):
+        """
+        Test that the tangent vectors of a right-angled triangle are computed correctly.
+        """
+        tangents = self.mover.tangents.dat.data
+        expected = [[1, 0], [np.sqrt(2) / 2, np.sqrt(2) / 2], [0, 1]]
+        self.assertTrue(np.allclose(np.abs(tangents), expected))
+
+    def test_angles(self):
+        """
+        Test that the arguments (angles from the x-axis) of a right-angled triangle are
+        computed correctly.
+        """
+        angles = self.rad2deg(self.mover.angles.dat.data)
+        self.assertTrue(np.allclose(angles, [0, 135, 90]))
