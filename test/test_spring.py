a7d52226d64067b36cd547afcf61b1fa338f435a
Joe Wallwork <joe.wallwork@outlook.com>
2024-06-19 08:29:18 +0100

    #19: Refactor spring movement tests

--- test/test_spring.py
+++ test/test_spring.py
@@ -146,50 +146,55 @@ class TestMovement(unittest.TestCase):
     Unit tests for movement under spring-based methods.
     """
 
+    @staticmethod
+    def move(mesh, fixed_boundary_tags=None, moving_boundary_tags=None, vector=[1, 0]):
+        mover = SpringMover(mesh, 1.0)
+        bcs = []
+        if fixed_boundary_tags:
+            bcs.append(DirichletBC(mover.coord_space, 0, fixed_boundary_tags))
+        if moving_boundary_tags:
+            f = Function(mover.coord_space).interpolate(as_vector(vector))
+            bcs.append(DirichletBC(mover.coord_space, f, moving_boundary_tags))
+        mover.move(0.0, boundary_conditions=bcs)
+        return mover.mesh
+
+    @staticmethod
+    def shifted_mesh(nx, ny, shift_x=0, shift_y=0):
+        mesh = UnitSquareMesh(nx, ny)
+        x, y = SpatialCoordinate(mesh)
+        shifted_coords = Function(mesh.coordinates)
+        shifted_coords.interpolate(as_vector([x + shift_x, y + shift_y]))
+        return Mesh(shifted_coords)
+
+    @staticmethod
+    def stretched_mesh(nx, ny, stretch_x=1, stretch_y=1):
+        return RectangleMesh(nx, ny, stretch_x, stretch_y)
+
     def test_fixed_triangle(self):
         mesh = UnitTriangleMesh()
         coords = mesh.coordinates.dat.data
-        mover = SpringMover(mesh, 1.0)
-        mover.move(0.0)
-        self.assertTrue(np.allclose(coords, mover.mesh.coordinates.dat.data))
+        self.assertTrue(np.allclose(coords, self.move(mesh).coordinates.dat.data))
 
     def test_fixed_square(self):
         mesh = UnitSquareMesh(1, 1)
         coords = mesh.coordinates.dat.data
-        mover = SpringMover(mesh, 1.0)
-        mover.move(0.0)
-        self.assertTrue(np.allclose(coords, mover.mesh.coordinates.dat.data))
+        self.assertTrue(np.allclose(coords, self.move(mesh).coordinates.dat.data))
 
     def test_force_right_free(self):
         mesh = UnitSquareMesh(10, 10)
         coord_array = mesh.coordinates.dat.data
-        mover = SpringMover(mesh, 1.0)
-        right = interpolate(as_vector([1, 0]), mover.coord_space)
-        mover.move(0, boundary_conditions=DirichletBC(mover.coord_space, right, 2))
-        new_coord_array = mover.mesh.coordinates.dat.data
+        new_coord_array = self.move(mesh, moving_boundary_tags=1).coordinates.dat.data
         self.assertFalse(np.allclose(coord_array, new_coord_array))
-
-        original_mesh = UnitSquareMesh(10, 10)
-        x, y = SpatialCoordinate(original_mesh)
-        shifted_coords = Function(original_mesh.coordinates)
-        shifted_coords.interpolate(as_vector([x + 1, y]))
-        shifted_mesh = Mesh(shifted_coords)
-        shifted_coord_array = shifted_mesh.coordinates.dat.data
+        shifted_coord_array = self.shifted_mesh(10, 10, shift_x=1).coordinates.dat.data
         self.assertTrue(np.allclose(shifted_coord_array, new_coord_array))
 
     def test_force_right_fixed(self):
         mesh = UnitSquareMesh(10, 10)
         coord_array = mesh.coordinates.dat.data
-        mover = SpringMover(mesh, 1.0)
-        right = interpolate(as_vector([1, 0]), mover.coord_space)
-        moving_bc = DirichletBC(mover.coord_space, right, 2)
-        fixed_bc = DirichletBC(mover.coord_space, 0, 1)
-        mover.move(0, boundary_conditions=[fixed_bc, moving_bc])
-        new_coord_array = mover.mesh.coordinates.dat.data
+        new_mesh = self.move(mesh, moving_boundary_tags=1, fixed_boundary_tags=2)
+        new_coord_array = new_mesh.coordinates.dat.data
         self.assertFalse(np.allclose(coord_array, new_coord_array))
-
         # # TODO: Implement no-slip BCs for segments 3 and 4 (#99)
-        # stretched_mesh = RectangleMesh(10, 10, 2, 1)
+        # stretched_mesh = self.stretched_mesh(10, 10, stretch_x=2)
         # stretched_coord_array = stretched_mesh.coordinates.dat.data
-        # plot_meshes(mover.mesh, stretched_mesh)
         # self.assertTrue(np.allclose(stretched_coord_array, new_coord_array))
