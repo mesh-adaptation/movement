14d32185f697deba0b047645afaccbceddf9e0a0
Joe Wallwork <joe.wallwork@outlook.com>
2024-12-16 16:59:15 +0000

    Update Monge-Ampere BC test

--- test/test_monge_ampere.py
+++ test/test_monge_ampere.py
@@ -292,22 +292,22 @@ class TestBCs(BaseClasses.TestMongeAmpere):
     @parameterized.expand(
         [
             (1, "relaxation", "on_boundary"),
-            (1, "relaxation", 1),
+            (1, "relaxation", [1]),
             (1, "relaxation", []),
             (1, "quasi_newton", "on_boundary"),
-            (1, "quasi_newton", 1),
+            (1, "quasi_newton", [1]),
             (1, "quasi_newton", []),
             (2, "relaxation", "on_boundary"),
-            (2, "relaxation", 1),
+            (2, "relaxation", [1]),
             (2, "relaxation", []),
             (2, "quasi_newton", "on_boundary"),
-            (2, "quasi_newton", 1),
+            (2, "quasi_newton", [1]),
             (2, "quasi_newton", []),
             (3, "relaxation", "on_boundary"),
-            (3, "relaxation", 1),
+            (3, "relaxation", [1]),
             (3, "relaxation", []),
             (3, "quasi_newton", "on_boundary"),
-            (3, "quasi_newton", 1),
+            (3, "quasi_newton", [1]),
             (3, "quasi_newton", []),
         ]
     )
@@ -330,16 +330,16 @@ class TestBCs(BaseClasses.TestMongeAmpere):
     @parameterized.expand(
         [
             (2, "relaxation", ["on_boundary"]),
-            (2, "relaxation", 1),
+            (2, "relaxation", [1]),
             (2, "relaxation", []),
             (2, "quasi_newton", ["on_boundary"]),
-            (2, "quasi_newton", 1),
+            (2, "quasi_newton", [1]),
             (2, "quasi_newton", []),
             (3, "relaxation", ["on_boundary"]),
-            (3, "relaxation", 1),
+            (3, "relaxation", [1]),
             (3, "relaxation", []),
             (3, "quasi_newton", ["on_boundary"]),
-            (3, "quasi_newton", 1),
+            (3, "quasi_newton", [1]),
             (3, "quasi_newton", []),
         ]
     )
