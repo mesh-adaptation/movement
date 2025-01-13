5281eb5842d1b11a91dc944fb5a59ccffba92126
Joe Wallwork <22053413+jwallwork23@users.noreply.github.com>
2025-01-13 18:47:52 +0000

    Implement `MeshTanglingChecker` in 1D and 3D (#142)

--- movement/tangling.py
+++ movement/tangling.py
@@ -2,7 +2,7 @@ import warnings
 
 import firedrake
 import ufl
-from firedrake.__future__ import interpolate
+from firedrake.petsc import PETSc
 
 __all__ = ["MeshTanglingChecker"]
 
@@ -21,44 +21,29 @@ class MeshTanglingChecker:
             otherwise a warning is raised
         :type raise_error: :class:`bool`
         """
-        if mesh.topological_dimension() != 2:
-            raise NotImplementedError("Tangling check only currently supported in 2D.")
         self.mesh = mesh
         self.raise_error = raise_error
-
-        # Store initial signs of Jacobian determinant
         P0 = firedrake.FunctionSpace(mesh, "DG", 0)
         detJ = ufl.JacobianDeterminant(mesh)
-        s = firedrake.assemble(interpolate(ufl.sign(detJ), P0))
-
-        # Get scaled Jacobian expression
-        P0_ten = firedrake.TensorFunctionSpace(mesh, "DG", 0)
-        J = firedrake.assemble(interpolate(ufl.Jacobian(mesh), P0_ten))
-        edge1 = ufl.as_vector([J[0, 0], J[1, 0]])
-        edge2 = ufl.as_vector([J[0, 1], J[1, 1]])
-        edge3 = edge1 - edge2
-        norm1 = ufl.sqrt(ufl.dot(edge1, edge1))
-        norm2 = ufl.sqrt(ufl.dot(edge2, edge2))
-        norm3 = ufl.sqrt(ufl.dot(edge3, edge3))
-        prod1 = ufl.max_value(norm1 * norm2, norm1 * norm3)
-        prod2 = ufl.max_value(norm2 * norm3, norm2 * norm1)
-        prod3 = ufl.max_value(norm3 * norm1, norm3 * norm2)
-        maxval = ufl.max_value(ufl.max_value(prod1, prod2), prod3)
-        self._sj_expr = detJ / maxval * s
-        self._sj = firedrake.Function(P0)
+        self._detJ_ratio_expr = detJ / firedrake.Function(P0).interpolate(detJ)
+        self._detJ_ratio = firedrake.Function(P0)
 
     @property
-    def scaled_jacobian(self):
-        self._sj.interpolate(self._sj_expr)
-        return self._sj
+    def jacobian_determinant_ratio(self):
+        """
+        Compute the ratio of the determinant of the Jacobian of the current mesh to
+        the determinant of the Jacobian of the original mesh.
+        """
+        return self._detJ_ratio.interpolate(self._detJ_ratio_expr)
 
+    @PETSc.Log.EventDecorator()
     def check(self):
         """
         Check whether any element orientations have changed since the tangling checker
         was created.
         """
-        sj = self.scaled_jacobian.dat.data_with_halos
-        num_tangled = len(sj[sj < 0])
+        jdr = self.jacobian_determinant_ratio.dat.data_with_halos
+        num_tangled = len(jdr[jdr <= 0])
         if num_tangled > 0:
             plural = "s" if num_tangled > 1 else ""
             msg = f"Mesh has {num_tangled} tangled element{plural}."
--- test/test_tangling.py
+++ test/test_tangling.py
@@ -1,6 +1,7 @@
 import unittest
 
-from firedrake.utility_meshes import UnitIntervalMesh, UnitSquareMesh
+import ufl
+from firedrake.utility_meshes import UnitCubeMesh, UnitIntervalMesh, UnitSquareMesh
 
 from movement.tangling import MeshTanglingChecker
 
@@ -10,34 +11,85 @@ class TestTangling(unittest.TestCase):
     Unit tests for mesh tangling checking.
     """
 
-    def test_dim_notimplementederror(self):
-        with self.assertRaises(NotImplementedError) as cm:
-            MeshTanglingChecker(UnitIntervalMesh(1))
-        msg = "Tangling check only currently supported in 2D."
-        self.assertEqual(str(cm.exception), msg)
-
-    def test_tangling_checker_error1(self):
-        checker = MeshTanglingChecker(UnitSquareMesh(3, 3), raise_error=True)
-        checker.mesh.coordinates.dat.data[3] += 0.2
+    def test_1_tangled_element_1d_error(self):
+        checker = MeshTanglingChecker(UnitIntervalMesh(3), raise_error=True)
+        checker.mesh.coordinates.dat.data[1] += 0.4  # Vertex 1: (1/3)
         with self.assertRaises(ValueError) as cm:
             checker.check()
         self.assertEqual(str(cm.exception), "Mesh has 1 tangled element.")
 
-    def test_tangling_checker_error2(self):
-        checker = MeshTanglingChecker(UnitSquareMesh(3, 3), raise_error=True)
-        checker.mesh.coordinates.dat.data[3] += 0.5
-        with self.assertRaises(ValueError) as cm:
-            checker.check()
-        self.assertEqual(str(cm.exception), "Mesh has 3 tangled elements.")
+    def test_1_tangled_element_1d(self):
+        checker = MeshTanglingChecker(UnitIntervalMesh(3), raise_error=False)
+        self.assertEqual(checker.check(), 0)
+        checker.mesh.coordinates.dat.data[1] += 0.4  # Vertex 1: (1/3)
+        self.assertEqual(checker.check(), 1)
 
-    def test_tangling_checker_dim_error(self):
-        with self.assertRaises(NotImplementedError) as cm:
-            MeshTanglingChecker(UnitIntervalMesh(1))
-        msg = "Tangling check only currently supported in 2D."
-        self.assertEqual(str(cm.exception), msg)
+    def test_2_tangled_elements_1d(self):
+        checker = MeshTanglingChecker(UnitIntervalMesh(3), raise_error=False)
+        checker.mesh.coordinates.dat.data[1] += 0.4  # Vertex 1: (1/3)
+        checker.mesh.coordinates.dat.data[3] -= 0.4  # Vertex 3: (1)
+        self.assertEqual(checker.check(), 2)
 
-    def test_tangling_checker_warning1(self):
+    def test_flip_1d(self):
+        checker = MeshTanglingChecker(UnitIntervalMesh(3), raise_error=False)
+        (x,) = ufl.SpatialCoordinate(checker.mesh)
+        checker.mesh.coordinates.interpolate(ufl.as_vector([1 - x]))
+        self.assertEqual(checker.check(), checker.mesh.num_cells())
+
+    def test_1_tangled_element_2d(self):
         checker = MeshTanglingChecker(UnitSquareMesh(3, 3), raise_error=False)
         self.assertEqual(checker.check(), 0)
-        checker.mesh.coordinates.dat.data[3] += 0.2
+        checker.mesh.coordinates.dat.data[3] += 0.2  # Vertex 3: (1/3, 1/3)
         self.assertEqual(checker.check(), 1)
+
+    def test_2_tangled_elements_2d(self):
+        checker = MeshTanglingChecker(UnitSquareMesh(3, 3), raise_error=False)
+        checker.mesh.coordinates.dat.data[3][0] += 0.5  # Vertex 3: (1/3, 1/3)
+        self.assertEqual(checker.check(), 2)
+
+    def test_3_tangled_elements_2d(self):
+        checker = MeshTanglingChecker(UnitSquareMesh(3, 3), raise_error=False)
+        checker.mesh.coordinates.dat.data[3] += 0.5  # Vertex 3: (1/3, 1/3)
+        self.assertEqual(checker.check(), 3)
+
+    def test_flip_2d(self):
+        checker = MeshTanglingChecker(UnitSquareMesh(3, 3), raise_error=False)
+        x, y = ufl.SpatialCoordinate(checker.mesh)
+        checker.mesh.coordinates.interpolate(ufl.as_vector([1 - x, y]))
+        self.assertEqual(checker.check(), checker.mesh.num_cells())
+
+    def test_double_flip_2d(self):
+        checker = MeshTanglingChecker(UnitSquareMesh(3, 3), raise_error=False)
+        x, y = ufl.SpatialCoordinate(checker.mesh)
+        checker.mesh.coordinates.interpolate(ufl.as_vector([1 - x, 1 - y]))
+        self.assertEqual(checker.check(), 0)
+
+    def test_2_tangled_elements_3d(self):
+        checker = MeshTanglingChecker(UnitCubeMesh(3, 3, 3), raise_error=False)
+        self.assertEqual(checker.check(), 0)
+        checker.mesh.coordinates.dat.data[14][2] -= 0.4  # Vertex 14: (0, 0, 1)
+        self.assertEqual(checker.check(), 2)
+
+    def test_6_tangled_elements_3d(self):
+        checker = MeshTanglingChecker(UnitCubeMesh(3, 3, 3), raise_error=False)
+        self.assertEqual(checker.check(), 0)
+        checker.mesh.coordinates.dat.data[33][0] += (0.4,)  # Vertex 33: (1/3, 1/3, 1/3)
+        self.assertEqual(checker.check(), 6)
+
+    def test_flip_3d(self):
+        checker = MeshTanglingChecker(UnitCubeMesh(3, 3, 3), raise_error=False)
+        x, y, z = ufl.SpatialCoordinate(checker.mesh)
+        checker.mesh.coordinates.interpolate(ufl.as_vector([1 - x, y, z]))
+        self.assertEqual(checker.check(), checker.mesh.num_cells())
+
+    def test_double_flip_3d(self):
+        checker = MeshTanglingChecker(UnitCubeMesh(3, 3, 3), raise_error=False)
+        x, y, z = ufl.SpatialCoordinate(checker.mesh)
+        checker.mesh.coordinates.interpolate(ufl.as_vector([1 - x, 1 - y, z]))
+        self.assertEqual(checker.check(), 0)
+
+    def test_triple_flip_3d(self):
+        checker = MeshTanglingChecker(UnitCubeMesh(3, 3, 3), raise_error=False)
+        x, y, z = ufl.SpatialCoordinate(checker.mesh)
+        checker.mesh.coordinates.interpolate(ufl.as_vector([1 - x, 1 - y, 1 - z]))
+        self.assertEqual(checker.check(), checker.mesh.num_cells())
