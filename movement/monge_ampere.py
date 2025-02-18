f3225dc3167a933895058b6d4ad81d7d486fb1a9
Davor Dundovic <33790330+ddundo@users.noreply.github.com>
2025-02-18 17:00:14 +0000

    Fix volume in MA demos’ callback (#156)
    Closes #155. 
    
    This got broken in #152.

--- movement/monge_ampere.py
+++ movement/monge_ampere.py
@@ -9,7 +9,7 @@ import firedrake
 import firedrake.exceptions as fexc
 import numpy as np
 import ufl
-from animate.utility import function2cofunction
+from animate.utility import cofunction2function, function2cofunction
 from firedrake.petsc import PETSc
 from pyadjoint import no_annotations
 
@@ -504,8 +504,10 @@ class MongeAmpereMover_Relaxation(MongeAmpereMover_Base):
             # Update monitor function
             self.to_physical_coordinates()
             self.monitor.interpolate(self.monitor_function(self.mesh))
-            firedrake.assemble(self.L_P0, tensor=function2cofunction(self.volume))
-            self.volume.interpolate(self.volume / self.original_volume)
+            _volume = function2cofunction(self.volume)
+            firedrake.assemble(self.L_P0, tensor=_volume)
+            _volume = cofunction2function(_volume)
+            self.volume.interpolate(_volume / self.original_volume)
             self.to_computational_coordinates()
 
             # Evaluate normalisation coefficient
@@ -708,8 +710,10 @@ class MongeAmpereMover_QuasiNewton(MongeAmpereMover_Base):
             cursol = snes.getSolution()
             update_monitor(cursol)
             self.to_physical_coordinates()
-            firedrake.assemble(self.L_P0, tensor=function2cofunction(self.volume))
-            self.volume.interpolate(self.volume / self.original_volume)
+            _volume = function2cofunction(self.volume)
+            firedrake.assemble(self.L_P0, tensor=_volume)
+            _volume = cofunction2function(_volume)
+            self.volume.interpolate(_volume / self.original_volume)
             self.to_computational_coordinates()
             PETSc.Sys.Print(
                 f"{i:4d}"
